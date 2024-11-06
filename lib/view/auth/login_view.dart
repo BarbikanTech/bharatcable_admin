765407d7fbc359bc0d53bffaa581fa88fb2e91ff
meena-barbikan <Selvameena1282003@gmail.com>
2024-11-06 12:09:44 +0530

    commit

--- lib/view/auth/login_view.dart
+++ lib/view/auth/login_view.dart
@@ -1,3 +1,167 @@
+// import 'package:flutter/material.dart';
+
+// class LoginView extends StatefulWidget {
+//   const LoginView({super.key});
+
+//   @override
+//   State<LoginView> createState() => _LoginViewState();
+// }
+
+// class _LoginViewState extends State<LoginView> {
+//   final _formKey = GlobalKey<FormState>();
+//   bool _rememberMe = false;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SingleChildScrollView(
+//         child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+//           ClipRRect(
+//             borderRadius: const BorderRadius.vertical(
+//               bottom: Radius.circular(20.0),
+//             ),
+//             child: Container(
+//               height: 100, // Set your preferred height here
+//               decoration: const BoxDecoration(
+//                 color: Color(0xffe0b21b),
+//               ),
+//               child: AppBar(
+//                 toolbarHeight:
+//                     80, // Match the AppBar height to the Container height
+//                 backgroundColor: Colors.transparent, // Make AppBar transparent
+//                 title: const Text('Bharath Cable'),
+//                 // Center the title
+//               ),
+//             ),
+//           ),
+//           const SizedBox(height: 70),
+//           const Padding(
+//             padding: EdgeInsets.all(10.0),
+//             child: Padding(
+//               padding: EdgeInsets.all(15.0),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   SizedBox(
+//                     height: 30,
+//                     child: Text(
+//                       'Welcome Back  ',
+//                       style: TextStyle(
+//                         fontSize: 24.0,
+//                         fontWeight: FontWeight.bold,
+//                       ),
+//                     ),
+//                   ),
+//                   SizedBox(
+//                       height: 40,
+//                       child: Text(
+//                         "Please enter your details",
+//                         style: TextStyle(
+//                           fontSize: 16.0,
+//                         ),
+//                       ))
+//                 ],
+//               ),
+//             ),
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.symmetric(horizontal: 20.0),
+//             child: Form(
+//               key: _formKey,
+//               child: Column(children: [
+//                 TextFormField(
+//                   decoration: const InputDecoration(
+//                     labelText: 'Email Address',
+//                     border: OutlineInputBorder(),
+//                   ),
+//                   keyboardType: TextInputType.emailAddress,
+//                   validator: (value) {
+//                     if (value == null || value.isEmpty) {
+//                       return 'Please enter your email';
+//                     }
+//                     return null;
+//                   },
+//                 ),
+//                 const SizedBox(height: 40), // Space between fields
+
+//                 // Password TextFormField
+//                 TextFormField(
+//                   decoration: const InputDecoration(
+//                     labelText: 'Password',
+//                     border: OutlineInputBorder(),
+//                   ),
+//                   obscureText: true, // For password input
+//                   validator: (value) {
+//                     if (value == null || value.isEmpty) {
+//                       return 'Please enter your password';
+//                     }
+//                     return null;
+//                   },
+//                 ),
+//                 SizedBox(
+//                   height: 80,
+//                   child: Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                     children: [
+//                       Row(
+//                         children: [
+//                           Checkbox(
+//                             value: _rememberMe,
+//                             onChanged: (value) {
+//                               setState(() {
+//                                 _rememberMe =
+//                                     value ?? false; // Toggle remember me
+//                               });
+//                             },
+//                           ),
+//                           const Text('Remember Me'),
+//                         ],
+//                       ),
+//                       TextButton(
+//                         onPressed: () {
+//                           // Handle forget password action
+//                         },
+//                         child: const Text(
+//                           'Forgot Password?',
+//                           style: TextStyle(
+//                             color: Colors.blue,
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//                 SizedBox(
+//                   height: 60,
+//                   width: double.infinity,
+//                   child: ElevatedButton(
+//                     onPressed: () {
+//                       // Handle login action here
+//                     },
+//                     style: ElevatedButton.styleFrom(
+//                       padding: const EdgeInsets.symmetric(vertical: 15.0),
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(10.0),
+//                       ),
+//                       backgroundColor: const Color(0xffe0b21b),
+//                     ),
+//                     child: const Text(
+//                       'Login',
+//                       style: TextStyle(
+//                         fontSize: 18.0,
+//                         color: Colors.black,
+//                       ),
+//                     ),
+//                   ),
+//                 )
+//               ]),
+//             ),
+//           ),
+//         ]),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
@@ -8,156 +172,19 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-  final _formKey = GlobalKey<FormState>();
-  bool _rememberMe = false;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          ClipRRect(
-            borderRadius: const BorderRadius.vertical(
-              bottom: Radius.circular(20.0),
-            ),
-            child: Container(
-              height: 100, // Set your preferred height here
-              decoration: const BoxDecoration(
-                color: Color(0xffe0b21b),
-              ),
-              child: AppBar(
-                toolbarHeight:
-                    80, // Match the AppBar height to the Container height
-                backgroundColor: Colors.transparent, // Make AppBar transparent
-                title: const Text('Bharath Cable'),
-                // Center the title
-              ),
-            ),
-          ),
-          const SizedBox(height: 70),
-          const Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 30,
-                    child: Text(
-                      'Welcome Back  ',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                      height: 40,
-                      child: Text(
-                        "Please enter your details",
-                        style: TextStyle(
-                          fontSize: 16.0,
-                        ),
-                      ))
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Form(
-              key: _formKey,
-              child: Column(children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Email Address',
-                    border: OutlineInputBorder(),
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your email';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 40), // Space between fields
-
-                // Password TextFormField
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                  obscureText: true, // For password input
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(
-                  height: 80,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Checkbox(
-                            value: _rememberMe,
-                            onChanged: (value) {
-                              setState(() {
-                                _rememberMe =
-                                    value ?? false; // Toggle remember me
-                              });
-                            },
-                          ),
-                          const Text('Remember Me'),
-                        ],
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          // Handle forget password action
-                        },
-                        child: const Text(
-                          'Forgot Password?',
-                          style: TextStyle(
-                            color: Colors.blue,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 60,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Handle login action here
-                    },
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(vertical: 15.0),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      backgroundColor: const Color(0xffe0b21b),
-                    ),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                )
-              ]),
-            ),
-          ),
-        ]),
+      appBar: AppBar(
+        title: const Image(image: AssetImage("assets/logo.png")),
+        centerTitle: true,
+      ),
+      body: Container(
+        height: 100,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            //color: Colors.black,
+            image: DecorationImage(image: AssetImage("assets/cable.png"))),
       ),
     );
   }
