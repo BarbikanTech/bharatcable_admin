0408a6de66a8d3740c9f3ed99a9ebc0b0d0742e9
sankar032 <msankar032@gmail.com>
2024-11-07 13:31:48 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       //home: isLogin ? const LandingView() : const LoginView(),
-      home: LoginView(),
+      home: const LoginView(),
     );
   }
 }
